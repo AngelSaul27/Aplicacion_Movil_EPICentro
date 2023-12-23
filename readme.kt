9b23aab261337010d82b5e8037e600ce16935a73
Ángel Saúl P. C <64120319+AngelSaul27@users.noreply.github.com>
2023-12-23 15:11:02 -0600

    Update readme.kt

--- readme.kt
+++ readme.kt
@@ -1,14 +1,29 @@
 package mx.gob.centro.epicentro
 
-/**
- *  README
- *  @author Angel Saul
- *  @lasModified 23-12-2023
- * */
-class Aplication : Application(){
+import android.content.Context
 
-    override fun onCreate() {
-        super.onCreate()
+class readme {
+
+    /**
+     * Convierte un valor en densidad de píxeles (dp) a píxeles (px) según la densidad de la pantalla.
+     */
+    fun dpToPx(context: Context, dp: Float) : Float {
+        return dp * context.resources.displayMetrics.density
     }
+
+    /**
+     * Convierte un valor en píxeles (px) a densidad de píxeles (dp) según la densidad de la pantalla.
+     */
+    fun pxToDp(context: Context, pxValue: Float): Float {
+        val density = context.resources.displayMetrics.density
+        return pxValue / density
+    }
+
+    fun removeAccents(input: String): String {
+        val regex = "\\p{InCombiningDiacriticalMarks}+".toRegex()
+        val temp = java.text.Normalizer.normalize(input, java.text.Normalizer.Form.NFD)
+        return regex.replace(temp, "")
+    }
+
 
 }
