2adb4cd27df6b1faa017b50f5db5782f7a5b082b
Ángel Saúl P. C <64120319+AngelSaul27@users.noreply.github.com>
2023-12-23 15:07:22 -0600

    Create readme.kt

---
+++ readme.kt
@@ -0,0 +1,14 @@
+package mx.gob.centro.epicentro
+
+/**
+ *  README
+ *  @author Angel Saul
+ *  @lasModified 23-12-2023
+ * */
+class Aplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
